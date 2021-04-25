dc2fd121cc2cbd00ba9662fca92da18598acf614
Tim Romanowski <tim.romanowski@naturalpoint.com>
2021-04-25 15:40:38 -0700

    Made it so you can move the potato with the arrow keys!!! :DDDDDDDDDD =DDDDDDD =3 :3 :P =P :) =)

--- res/javascript/potato.js
+++ res/javascript/potato.js
@@ -1,12 +1,35 @@
 // document ready
 $(function() {
 
-    $( document ).mousedown(function(evt) {
-       $("#smth").animate({top: evt.offsetY, left: evt.offsetX});
-       var colorR = Math.floor((Math.random() * 256));
-       var colorG = Math.floor((Math.random() * 256));
-       var colorB = Math.floor((Math.random() * 256));
-       $("#colorhehe").css("background-color", "rgb(" + colorR + "," + colorG + "," + colorB + ")");
+
+    $(document).mousedown(function (evt) {
+        $("#smth").animate({top: evt.offsetY, left: evt.offsetX});
+        var colorR = Math.floor((Math.random() * 256));
+        var colorG = Math.floor((Math.random() * 256));
+        var colorB = Math.floor((Math.random() * 256));
+        $("#colorhehe").css("background-color", "rgb(" + colorR + "," + colorG + "," + colorB + ")");
     });
 
+
+    let moveBy = 10;
+
+
+    $(document).keydown(function (evt) {
+        var potato =  document.querySelector('#smth');
+        switch (evt.key) {
+            case 'ArrowLeft' :
+                potato.style.left = parseInt(potato.style.left) - moveBy + 'px';
+                break;
+            case 'ArrowRight' :
+                potato.style.left = parseInt(potato.style.left) + moveBy + 'px';
+                break;
+            case 'ArrowUp' :
+                potato.style.top = parseInt(potato.style.top) - moveBy + 'px';
+                break;
+            case 'ArrowDown' :
+                potato.style.top = parseInt(potato.style.top) + moveBy + 'px';
+                break;
+
+        }
+    });
 });
