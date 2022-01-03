3536013f4b30edde3de45012dcee9c03c7d67c4a
kira <kira.romanowski@gmail.com>
2022-01-02 21:33:25 -0800

    I added a score at the top of the screen. It goes up every time the even mousedown occurs. I also made it so that it looks normal on both mobile and on a computer.

--- res/javascript/potato.js
+++ res/javascript/potato.js
@@ -1,18 +1,28 @@
 // document ready
 $(function() {
 
+
+    var score = 0;
     var isFirstClick = true;
+
     $(document).mousedown(function (evt) {
         if (isFirstClick) {
            $('.outerWindow').hide();
            $('#colorhehe').show();
+           $('.scorebox').show();
          isFirstClick = false;
         } else {
+        score = score + 1;
+           $('#score').text(score)
            $("#smth").animate({top: evt.offsetY, left: evt.offsetX});
             var colorR = Math.floor((Math.random() * 256));
             var colorG = Math.floor((Math.random() * 256));
             var colorB = Math.floor((Math.random() * 256));
             $("#colorhehe").css("background-color", "rgb(" + colorR + "," + colorG + "," + colorB + ")");
+            var colorR = Math.floor((Math.random() * 356));
+            var colorG = Math.floor((Math.random() * 356));
+            var colorB = Math.floor((Math.random() * 356));
+            $("#score").css("color","rgb(" + colorR + "," + colorG + "," + colorB + ")");
         }
     });
 
